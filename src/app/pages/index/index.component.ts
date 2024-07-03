2d70d86703d1f05213e0508540f8690bbe0b3a03
Felipesdr <felipederose@gmail.com>
2024-07-03 12:23:04 -0300

    Sessão de contatos adicionada

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { TopLayoutComponent } from '../../components/top-layout/top-layout.component';
 import { ArtigosLayoutComponent } from '../../components/artigos-layout/artigos-layout.component';
+import { FooterLayoutComponent } from '../../components/footer-layout/footer-layout.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
   imports: [
     TopLayoutComponent,
-    ArtigosLayoutComponent
+    ArtigosLayoutComponent,
+    FooterLayoutComponent
   ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
